f5f8b2214af6c78bd7e608e2f25a01174d9c1e40
akcasey <akc.997+github@gmail.com>
2017-05-02 07:44:41 -0700

    5.2.17-1

--- course/course-details-scripts.js
+++ course/course-details-scripts.js
@@ -1,75 +1,51 @@
-// - - - - Animate on Scroll - - - - 
-        AOS.init({
-            duration: 400,
-            easing: 'ease-in-sine',
-            delay: 100,
-        });
-  
-// - - - - Materialize Initialization - - - -
-        (function($){
-            $(function(){
-                $('.button-collapse').sideNav();
-                $('.collapsible').collapsible();
-                $('ul.tabs').tabs();
-                $('.modal-trigger').leanModal();
-                $('.materialboxed').materialbox();
-            });
-        })(jQuery);
-
-// - - - - Side Navigation - - - -
-        $(function(){
-            var $image = $('#sideprofile-photo').clone();
-            $('#profile-photo-topnav').html($image);
-        });
-
-        document.getElementById('sideprofile-name').appendChild(document.getElementById('profileItemUserData'));
-        document.getElementById('sideprofile-photo').appendChild(document.getElementById('imgUserThumb'));
-        document.getElementById('sideprofile-completedcourses').appendChild(document.getElementById('completionsItem'));
-        document.getElementById('sideprofile-completedcourses').appendChild(document.getElementById('enrolledCoursesItem'));
-        document.getElementById('admin-topnav').appendChild(document.getElementById('uni-frontside-banner-links'));
-
-// - - - - Maintenance Notification - - - -
-        $("#bv-maintenance-message").append($(".MaintenanceMessage"));
-
-        if($('.MaintenanceMessage').length){
-            $('#bv-maintenance-message-wrapper').show();
-        }
-
-// - - - - Tabs  - - - -
-        $(document).ready(function() {
-            $('ul.tabs').tabs();
-            $(".course-enrolled-link").click(function() { $('ul.tabs').tabs('select_tab', 'enrolled-courses'); });
-            $(".course-completed-link").click(function() { $('ul.tabs').tabs('select_tab', 'completed-courses'); });
-            $(".course-calendar-link").click(function() { $('ul.tabs').tabs('select_tab', 'calendar'); });
-            $(".course-categories-link").click(function() { $('ul.tabs').tabs('select_tab', 'categories'); });
-        });
-
-// - - - - Page Specific  - - - -
-        $(document).ready(function() {
-            $("#page-title").prepend($("#courseName"));                     // Relocating page name
-            $('.expandable').removeClass('expandable');                     // Remove uni-frontside-list-item hover class
-            $("#course-details").append($("#pnlCourseTitleCostButton"));
-            $("#course-details").append($("#AdditionalLinks"));
-            $("#course-details").append($("#pEnrollBottom"));
-            $("#course-details").append($("#btnUnEnroll"));
-
-
-            // Getting the url for the outline tab
-            /*var url      = window.location.href;
-            var coursename = url.match(/CourseDetail(.+)/)[1];
-                
-            $(function(){
-                $('#outline').load('https://go.bluevolt.com/A-dec/CourseOutline' + coursename + ' #courseOutlineContainer');
-            }); */
-        });   
-        // Fixing the radio buttons: assigning new id and adding label
-        $('input[type="radio"]').each(function (i) {
-            $(this).attr('id', 'rbSchedule' + i);
-            $(this).after("<label for='rbSchedule" + i + "'></label>");    
-        });
-
-        // When the Enroll button is clicked  
-        $("#btnEnroll").click(function () {
-            // Change id of selected radio button
-            $('input[name=schedules]:checked').attr('id', 'rbSchedule');
-        });
+AOS.init({duration:400, easing:"ease-in-sine", delay:100});
+(function(a) {
+  a(function() {
+    a(".button-collapse").sideNav();
+    a(".collapsible").collapsible();
+    a("ul.tabs").tabs();
+    a(".modal-trigger").leanModal();
+    a(".materialboxed").materialbox();
+  });
+})(jQuery);
+$(function() {
+  var a = $("#sideprofile-photo").clone();
+  $("#profile-photo-topnav").html(a);
+});
+document.getElementById("sideprofile-name").appendChild(document.getElementById("profileItemUserData"));
+document.getElementById("sideprofile-photo").appendChild(document.getElementById("imgUserThumb"));
+document.getElementById("sideprofile-completedcourses").appendChild(document.getElementById("completionsItem"));
+document.getElementById("sideprofile-completedcourses").appendChild(document.getElementById("enrolledCoursesItem"));
+document.getElementById("admin-topnav").appendChild(document.getElementById("uni-frontside-banner-links"));
+$("#bv-maintenance-message").append($(".MaintenanceMessage"));
+$(".MaintenanceMessage").length && $("#bv-maintenance-message-wrapper").show();
+$(document).ready(function() {
+  $("ul.tabs").tabs();
+  $(".course-enrolled-link").click(function() {
+    $("ul.tabs").tabs("select_tab", "enrolled-courses");
+  });
+  $(".course-completed-link").click(function() {
+    $("ul.tabs").tabs("select_tab", "completed-courses");
+  });
+  $(".course-calendar-link").click(function() {
+    $("ul.tabs").tabs("select_tab", "calendar");
+  });
+  $(".course-categories-link").click(function() {
+    $("ul.tabs").tabs("select_tab", "categories");
+  });
+});
+$(document).ready(function() {
+  $("#page-title").prepend($("#courseName"));
+  $(".expandable").removeClass("expandable");
+  $("#course-details").append($("#pnlCourseTitleCostButton"));
+  $("#course-details").append($("#AdditionalLinks"));
+  $("#course-details").append($("#pEnrollBottom"));
+  $("#course-details").append($("#btnUnEnroll"));
+});
+$('input[type="radio"]').each(function(a) {
+  $(this).attr("id", "rbSchedule" + a);
+  $(this).after("<label for='rbSchedule" + a + "'></label>");
+});
+$("#btnEnroll").click(function() {
+  $("input[name=schedules]:checked").attr("id", "rbSchedule");
+});
--- course/course-outline-scripts.js
+++ course/course-outline-scripts.js
@@ -1,87 +1,65 @@
-// - - - - Animate on Scroll - - - -
-        AOS.init({
-            duration: 400,
-            easing: 'ease-in-sine',
-            delay: 100,
-        });
-  
-// - - - - Materialize Initialization - - - -
-        (function($){
-            $(function(){
-                $('.button-collapse').sideNav();
-                $('.collapsible').collapsible();
-                $('ul.tabs').tabs();
-                $('.modal-trigger').leanModal();
-                $('.materialboxed').materialbox();
-            });
-        })(jQuery);
-
-// - - - - Side Navigation - - - -
-        $(function(){
-            var $image = $('#sideprofile-photo').clone();
-            $('#profile-photo-topnav').html($image);
-        });
-
-        document.getElementById('sideprofile-name').appendChild(document.getElementById('profileItemUserData'));
-        document.getElementById('sideprofile-photo').appendChild(document.getElementById('imgUserThumb'));
-        document.getElementById('sideprofile-completedcourses').appendChild(document.getElementById('completionsItem'));
-        document.getElementById('sideprofile-completedcourses').appendChild(document.getElementById('enrolledCoursesItem'));
-        document.getElementById('admin-topnav').appendChild(document.getElementById('uni-frontside-banner-links'));
-      
-// - - - - Maintenance Notification - - - -
-        $("#bv-maintenance-message").append($(".MaintenanceMessage"));
-
-        if($('.MaintenanceMessage').length){
-            $('#bv-maintenance-message-wrapper').show();
-        }
-
-// - - - - Tabs  - - - -
-        $(document).ready(function() {
-            $('ul.tabs').tabs();
-            $(".course-enrolled-link").click(function() { $('ul.tabs').tabs('select_tab', 'enrolled-courses'); });
-            $(".course-completed-link").click(function() { $('ul.tabs').tabs('select_tab', 'completed-courses'); });
-            $(".course-calendar-link").click(function() { $('ul.tabs').tabs('select_tab', 'calendar'); });
-            $(".course-categories-link").click(function() { $('ul.tabs').tabs('select_tab', 'categories'); });
-        });
-
-// - - - - Page Specific  - - - -
-        $(document).ready(function() {   
-            $("#page-title").prepend($("#courseName")); 
-            $('.expandable').removeClass('expandable');
-            $("#course-details-addtocalendar").prepend($("#courseOutlineContainer #aAddToCalendar"));    
-            $("#course-details-schedulestartend").prepend($("#courseOutlineContainer #scheduleStartEnd"));    
-            $("#course-details-location").prepend($("#courseOutlineContainer #pLocation"));
-            $("#course-details-instructor").prepend($("#courseOutlineContainer #pInstructor"));            
-
-            $("#course-details-enrollbottom").prepend($("#courseOutlineContainer #pEnrollBottom")); 
-            $("#course-details-enrollbottom").append($(".status-container")); 
-            $("#course-details-unenroll").prepend($("#courseOutlineContainer #btnUnEnroll"));  
-              
-            $("#course-details-toggleschedules").prepend($("#courseOutlineContainer #toggleSchedules"));
-            $("#course-details-otherschedules").prepend($("#courseOutlineContainer #divOtherSchedules"));     
-            $("#course-details-additionallinks").prepend($("#AdditionalLinks"));
-
-            $("#course-modules").prepend($("#rowSectionData"));      
-            $("#description").prepend($("#pnlCourseDetailsImageHolder"));
-            $("#flash-messages").append($(".flash-messages-container"));                  
-        });   
-
-
-        $(document).ready(function () {
-            // Stop the function window.refresher from executing (after launching modules)
-            window.refresher=function(){return false;};
-        });            
-
-
-
-        // Fixing the radio buttons: assigning new id and adding label
-        $('input[type="radio"]').each(function (i) {
-            $(this).attr('id', 'rbSchedule' + i);
-            $(this).after("<label for='rbSchedule" + i + "'></label>");    
-        });
-
-        // When the Enroll button is clicked  
-        $("#btnEnroll").click(function () {
-            // Change ID of selected radio button
-            $('input[name=schedules]:checked').attr('id', 'rbSchedule');
-        });
+AOS.init({duration:400, easing:"ease-in-sine", delay:100});
+(function(a) {
+  a(function() {
+    a(".button-collapse").sideNav();
+    a(".collapsible").collapsible();
+    a("ul.tabs").tabs();
+    a(".modal-trigger").leanModal();
+    a(".materialboxed").materialbox();
+  });
+})(jQuery);
+$(function() {
+  var a = $("#sideprofile-photo").clone();
+  $("#profile-photo-topnav").html(a);
+});
+document.getElementById("sideprofile-name").appendChild(document.getElementById("profileItemUserData"));
+document.getElementById("sideprofile-photo").appendChild(document.getElementById("imgUserThumb"));
+document.getElementById("sideprofile-completedcourses").appendChild(document.getElementById("completionsItem"));
+document.getElementById("sideprofile-completedcourses").appendChild(document.getElementById("enrolledCoursesItem"));
+document.getElementById("admin-topnav").appendChild(document.getElementById("uni-frontside-banner-links"));
+$("#bv-maintenance-message").append($(".MaintenanceMessage"));
+$(".MaintenanceMessage").length && $("#bv-maintenance-message-wrapper").show();
+$(document).ready(function() {
+  $("ul.tabs").tabs();
+  $(".course-enrolled-link").click(function() {
+    $("ul.tabs").tabs("select_tab", "enrolled-courses");
+  });
+  $(".course-completed-link").click(function() {
+    $("ul.tabs").tabs("select_tab", "completed-courses");
+  });
+  $(".course-calendar-link").click(function() {
+    $("ul.tabs").tabs("select_tab", "calendar");
+  });
+  $(".course-categories-link").click(function() {
+    $("ul.tabs").tabs("select_tab", "categories");
+  });
+});
+$(document).ready(function() {
+  $("#page-title").prepend($("#courseName"));
+  $(".expandable").removeClass("expandable");
+  $("#course-details-addtocalendar").prepend($("#courseOutlineContainer #aAddToCalendar"));
+  $("#course-details-schedulestartend").prepend($("#courseOutlineContainer #scheduleStartEnd"));
+  $("#course-details-location").prepend($("#courseOutlineContainer #pLocation"));
+  $("#course-details-instructor").prepend($("#courseOutlineContainer #pInstructor"));
+  $("#course-details-enrollbottom").prepend($("#courseOutlineContainer #pEnrollBottom"));
+  $("#course-details-enrollbottom").append($(".status-container"));
+  $("#course-details-unenroll").prepend($("#courseOutlineContainer #btnUnEnroll"));
+  $("#course-details-toggleschedules").prepend($("#courseOutlineContainer #toggleSchedules"));
+  $("#course-details-otherschedules").prepend($("#courseOutlineContainer #divOtherSchedules"));
+  $("#course-details-additionallinks").prepend($("#AdditionalLinks"));
+  $("#course-modules").prepend($("#rowSectionData"));
+  $("#description").prepend($("#pnlCourseDetailsImageHolder"));
+  $("#flash-messages").append($(".flash-messages-container"));
+});
+$(document).ready(function() {
+  window.refresher = function() {
+    return !1;
+  };
+});
+$('input[type="radio"]').each(function(a) {
+  $(this).attr("id", "rbSchedule" + a);
+  $(this).after("<label for='rbSchedule" + a + "'></label>");
+});
+$("#btnEnroll").click(function() {
+  $("input[name=schedules]:checked").attr("id", "rbSchedule");
+});
